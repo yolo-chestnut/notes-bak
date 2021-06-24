80e2f99cc157fa43cfc926b7fa38f1f46ad65313
yolo-chestnut <1015653498@qq.com>
2021-06-25 00:25:03 +0800

    zookeeper-ACL-digest模式，要求对用户名:密码，先由SHA-1加密，再使用BASE64编码，本程序为java代码实现

---
+++ java-aspect/src/pirv/yolo/zkencryption/ZkEncryption.java
@@ -0,0 +1,22 @@
+package pirv.yolo.zkencryption;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class ZkEncryption {
+
+    /**
+     * 使用SHA-1算法加密，然后由BASE64编码
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        // 字符串格式，形如 用户名:密码
+        String usernameAndPasswordStr = "username:password";
+        String algorithm = "SHA-1";
+        byte[] digest = MessageDigest.getInstance(algorithm).digest(usernameAndPasswordStr.getBytes(StandardCharsets.UTF_8));
+        String encryptStr = Base64.getEncoder().encodeToString(digest);
+        System.out.println(encryptStr);
+    }
+
+}
